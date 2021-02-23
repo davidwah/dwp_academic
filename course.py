6271e84c6e6a2654cc0c010cc11a2ca7a63f9a29
davidwah <davidwahyuyu@gmail.com>
2021-02-24 02:08:12 +0700

    upgrade field course

--- course.py
+++ course.py
@@ -8,5 +8,8 @@ class Course(models.Model):
     description = fields.Text(string="Description", required=False, )
     responsible_id = fields.Many2one(comodel_name="res.users", string="Responsible", required=True)
     
+    session_ids = fields.One2many(comodel_name="academic.session", string="Session",
+        inverse_name="course_id")
+
     
     
